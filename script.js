369722efde547c3a34fcf8a63cf9bde1b62f0c7a
crab79 <hanyaoyao0709@gmail.com>
2023-09-14 22:59:39 +0800

    Change Reading-button's word

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//添加關閉按鈕，用物件方式生成DOM
+//待辦：用物件方式生成DOM，添加關閉按鈕的功能，改變閱讀狀態
 const adding = document.getElementById("adding");
 const addBookDialog = document.getElementById("addBookDialog");
 const outputBox = document.getElementsByClassName("content")[0];
@@ -26,7 +26,7 @@ addBookDialog.addEventListener("close", (e) => {
     addAuthor(inputEl[1].value, infoClass);
     addPage(inputEl[2].value, infoClass);
     addDate(inputEl[3].value, infoClass);
-    addReadingButton(inputEl[4].checked ? "Yes" : "No", outputContainer);
+    addReadingButton(inputEl[4].checked ? "Read" : "Not yet", outputContainer);
 
     // Append the outputContainer to outputBox
     outputBox.appendChild(outputContainer);
@@ -42,36 +42,36 @@ confirmBtn.addEventListener("click", (event) => {
 });
 
 
-function addClosingButton(container){
+function addClosingButton(container) {
   const newClosingButton = closingButton.cloneNode(true);
   container.appendChild(newClosingButton);
 }
 
-function addReadingButton(content, container){
+function addReadingButton(content, container) {
   const newButton = document.createElement('button');
   newButton.textContent = content;
   container.appendChild(newButton);
 }
 
-function addTitle(content, container){
+function addTitle(content, container) {
   const title = document.createElement("h1");
   title.textContent = content;
   container.appendChild(title);
 }
 
-function addPage(content, container){
+function addPage(content, container) {
   const page = document.createElement("h3");
   page.textContent = "Page: " + content;
   container.appendChild(page);
 }
 
-function addDate(content, container){
+function addDate(content, container) {
   const date = document.createElement("h3");
   date.textContent = "Date: " + content;
   container.appendChild(date);
 }
 
-function addAuthor(content, container){
+function addAuthor(content, container) {
   const author = document.createElement("h3");
   author.textContent = "By " + content;
   author.setAttribute("id", "author");
